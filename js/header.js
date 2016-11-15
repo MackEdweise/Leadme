a9352453598a6cca942e41eafacb8c5dfa4d4ee2
Marcus Edwards <marcusedwards@hotmail.ca>
2016-11-15 11:20:03 -0500

    watch responsivity

---
+++ js/header.js
@@ -0,0 +1,18 @@
+/**
+ * Created by marcusedwards on 2016-11-15.
+ */
+
+$(document).ready(function() {
+    if($(window).width() < 770) {
+        $('.watch').hide();
+    }
+    $(window).resize(function () {
+        if($(window).width() < 770) {
+            $('header').css('height', '100vh');
+            $('.watch').hide();
+        }
+        else{
+            $('.watch').show();
+        }
+    });
+});
